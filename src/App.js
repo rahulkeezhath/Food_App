bd7a9a1b60b160e2fca48e38b2e94fb016f1b902
rahulkeezhath <rahulkeezhath.com>
2023-04-14 16:03:43 +0530

    Food Commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import Navbar from "./components/Navbar"
 import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <HeadlineCards/>
+      <Food/>
     </div>
   );
 }
